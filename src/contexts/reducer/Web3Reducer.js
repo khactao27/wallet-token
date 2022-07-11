5f3af1c3fc2525cddd607f1e8176931ef64b31aa
Tao D.K <tao.duongkhac@gmail.com>
2022-07-11 08:07:41 +0700

    add

--- src/contexts/reducer/Web3Reducer.js
+++ src/contexts/reducer/Web3Reducer.js
@@ -2,9 +2,33 @@ const initState = {
   account: "",
   web3: null,
   network: "",
+  connect: false,
+  balance: 0,
+  status: 'not connected'
 };
 
-function Web3Reducer(state, action) {}
+export const CONNECT = "connect"
+export const UPDATE_ACCOUNT = "update_account"
+export const UPDATE_NETWORK = "update_network"
+
+export const web3Action = (type, payload) => ({
+  type,
+  payload
+})
+
+function Web3Reducer(state, action) {
+  switch(action.type) {
+    case CONNECT:
+      return {...state, ...action.payload, account: "HEHHE", network: "Ethereum", connect: true, balance: "1000" }
+    case UPDATE_ACCOUNT:
+      return {}
+    case UPDATE_NETWORK:
+      return {}
+    default:
+      return state
+  }
+}
+
 
 export { initState };
 export default Web3Reducer;
--- src/pages/metamask/index.js
+++ src/pages/metamask/index.js
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
-import { Image, Button } from "antd";
+import React, { useState, useEffect } from "react";
+
+import { useWeb3Context } from "../../contexts/contexts/Web3Context";
+import { web3Action } from "../../contexts/reducer/Web3Reducer";
+import { CONNECT } from "../../contexts/reducer/Web3Reducer";
+
 
 const MetaMask = () => {
+    const [state, dispatch] = useWeb3Context();
+
   return (
     <div>
       <h1>Metamask</h1>
-      <Button>Connect</Button>
+      <p>account: {state.account}</p>
+      <p>Network: {state.network}</p>
+      <p>Balance: {state.balance}</p>
+{   !state.connect ? <button onClick={(event) => {
+        dispatch(web3Action(CONNECT, { name: "TAO", age: 12 }))
+    }}>Connect</button> : <div>Metamask connected</div>
+}
     </div>
   );
 };
